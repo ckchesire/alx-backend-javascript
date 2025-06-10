b4bbed949410924ea8aade597ed2689941f19816
Christian Chesire <christianck.chesire@gmail.com>
2025-06-11 02:13:08 +0300

    Updated HTTP server using Node's HTTP module.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,3 @@
-/*
-* Basic HTTP server
-*/
 const http = require('http');
 
 const app = http.createServer((req, res) => {
@@ -9,6 +6,8 @@ const app = http.createServer((req, res) => {
   res.end('Hello ALX!');
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
 
 module.exports = app;
