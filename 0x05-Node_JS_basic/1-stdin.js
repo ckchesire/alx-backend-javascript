f258bcf5cca3a717c3fb86cdf35145e9b0f5be43
Christian Chesire <christianck.chesire@gmail.com>
2025-06-10 19:47:10 +0300

    Updated 1-stdin.js that uses Process stdin.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,8 +9,13 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing.');
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing.\n');
+});
+
+process.on('SIGINT', () => {
+  process.exit();
 });
