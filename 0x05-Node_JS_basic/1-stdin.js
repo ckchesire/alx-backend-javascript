84dfbfffa3f0bcc3b2405de402daa92404dc72b1
Christian Chesire <christianck.chesire@gmail.com>
2025-06-10 20:21:12 +0300

    Updated Fix: 1-stdin.js that uses Process stdin.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,35 +1,12 @@
-/**
- * Program to take in user input from stdin and display the appropriate message
- */
-const readline = require('readline');
+/*
+* Program to take in user input from stdin and display the appropriate message
+*/
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
-const { isTTY } = process.stdin;
-
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: isTTY ? process.stdout : null,
-  terminal: isTTY,
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-function askUserName() {
-  if (!isTTY) {
-    process.stdout.write('Welcome to ALX, what is your name?\n');
-  }
-  r1.question('Welcome to ALX, what is your name?\n', (data) => {
-    const name = data.trim();
-    console.log(`Your name is: ${name}`);
-    r1.close();
-  });
-}
-
-r1.on('close', () => {
-  if (!isTTY) {
-    console.log('This important software is now closing');
-  }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-module.exports = askUserName;
-
-if (require.main === module) {
-  askUserName();
-}
