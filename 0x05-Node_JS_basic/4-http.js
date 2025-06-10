50006bbfe47e79a1156e1d65eb887b9167c3142b
Christian Chesire <christianck.chesire@gmail.com>
2025-06-11 02:05:23 +0300

    Implemented a HTTP server using Node's HTTP module.

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/*
+* Basic HTTP server
+*/
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.end('Hello ALX!');
+});
+
+app.listen(1245);
+
+module.exports = app;
