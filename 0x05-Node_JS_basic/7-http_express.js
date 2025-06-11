678da408f1b6b2089022ff2129ec94460a8cdf5e
Christian Chesire <christianck.chesire@gmail.com>
2025-06-11 03:39:13 +0300

    Implementation update for complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,78 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
-const PORT = 1245;
 
-const database = process.argv[2];
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+      if (lines.length <= 1) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const students = lines.slice(1);
+      const validStudents = students.filter((student) => student.trim() !== '');
+
+      if (validStudents.length === 0) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const fields = {};
+
+      validStudents.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+        if (firstname && field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
+      });
+
+      let result = `Number of students: ${validStudents.length}\n`;
+
+      for (const [field, names] of Object.entries(fields)) {
+        result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+      }
+
+      resolve(result.trim());
+    });
+  });
+}
 
 app.get('/', (req, res) => {
+  res.type('text/plain');
   res.send('Hello ALX!');
 });
 
 app.get('/students', async (req, res) => {
-  let response = 'This is a list of our students\n';
+  res.type('text/plain');
+
+  const databasePath = process.argv[2];
+
+  if (!databasePath) {
+    res.send('This is the list of our students\nCannot load the database');
+    return;
+  }
+
   try {
-    const lines = [];
-    const { log } = console;
-    console.log = (msg) => lines.push(msg);
-
-    await countStudents(database);
-
-    console.log = log;
-    response += lines.join('\n');
-    res.set('Conten-Type', 'text/plain');
-    res.send(response);
+    const studentsData = await countStudents(databasePath);
+    res.send(`This is the list of our students\n${studentsData}`);
   } catch (error) {
-    res.status(500).send('This is the list of our students\nCannot load the database');
+    res.send(`This is the list of our students\n${error.message}`);
   }
 });
 
-app.listen(PORT, () => {
+app.listen(1245, () => {
   console.log('Server running on port 1245');
 });
 
